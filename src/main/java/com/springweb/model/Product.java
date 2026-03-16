808097d9815e81ae2cc2688a49e36f6465042a20
narasimha <rasaninarasimha1@gmail.com>
2026-03-17 00:37:51 +0530

    Updated ProductController, Product model, ProductServices, and added repository package

--- src/main/java/com/springweb/model/Product.java
+++ src/main/java/com/springweb/model/Product.java
@@ -1,10 +1,14 @@
 package com.springweb.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Component;
 
 @Component
+@Entity
 public class Product {
 
+    @Id
     private int prodId;
     private String  prodName;
     private int price;
--- src/main/java/com/springweb/services/ProductServices.java
+++ src/main/java/com/springweb/services/ProductServices.java
@@ -1,6 +1,7 @@
 package com.springweb.services;
 
 import com.springweb.model.Product;
+import com.springweb.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,38 +13,45 @@ import java.util.List;
 @Service
 public class ProductServices {
 
+    @Autowired
+    ProductRepo repo;
 
-    List<Product> products =new ArrayList<>(Arrays.asList(
-            new Product(101, "Iphone", 50000),
-            new Product(102, "Samsung", 40000),
-            new Product(103, "OnePlus", 35000),
-            new Product(104, "Realme", 20000),
-            new Product(105, "Pixel", 60000)
-    ));
+
+//    List<Product> products =new ArrayList<>(Arrays.asList(
+//            new Product(101, "Iphone", 50000),
+//            new Product(102, "Samsung", 40000),
+//            new Product(103, "OnePlus", 35000),
+//            new Product(104, "Realme", 20000),
+//            new Product(105, "Pixel", 60000)
+//    ));
 
 
 
     public List<Product> getProducts(){
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId){
-        return products.stream()
-                .filter(p -> p.getProdId() == prodId)
-                .findFirst().get();
+
+        return repo.findById(prodId).orElse(new Product());
+//        return products.stream()
+//                .filter(p -> p.getProdId() == prodId)
+//                .findFirst().get();
     }
 
     public void addProduct(Product prod){
-        products.add(prod);
+
+        repo.save(prod);
     }
 
     public void updateProduct(Product prod) {
-        int index =0;
-        for(int i=0;i<products.size();i++){
-            if(products.get(i).getProdId() == prod.getProdId())
-                index = i;
-            products.set(index,prod);
+        repo.save(prod);
         }
 
+
+    public void deleteProduct(int prodId) {
+
+        repo.deleteById(prodId);
     }
 }
+
